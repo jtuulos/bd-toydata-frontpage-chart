ff9b1dc8ca6fd54040388bbac8d161fa98e7d740
Ville Tuulos <ville@bitdeli.com>
2012-11-22 00:39:29 +0000

    add a line chart

--- __main__.py
+++ __main__.py
@@ -1,9 +1,29 @@
 import bitdeli
-from bitdeli.widgets import Map, set_theme
+from bitdeli.widgets import Line, set_theme
+from datetime import datetime, timedelta
+import random
 
-set_theme('eighties')
+set_theme('bluered')
+
+random.seed(1)
+
+def month():
+    first = datetime.today()
+    for i in range(30):
+        yield (first + timedelta(days=i)).isoformat()
+
+def data_exp():
+    for i, date in enumerate(month()):
+         yield date, (2**(i * 0.2) + random.gauss(10, 2)) * 1000
+
+def data_lin():
+    for i, date in enumerate(month()):
+        yield date, (i + random.gauss(10, 2)) * 1000
 
 for profile in bitdeli.profiles():
     pass
 
-Map(size=(12, 7), data={'IN': 30})
+Line(size=(10, 7),
+     data=[{'label': 'Android users', 'data': list(data_exp())},
+           {'label': 'IPhone users', 'data': list(data_exp())},
+           {'label': 'Web users', 'data': list(data_lin())}])
